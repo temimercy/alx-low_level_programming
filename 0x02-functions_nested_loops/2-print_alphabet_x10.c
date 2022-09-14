2a3c86668ff1bdfe5e42ffd36ffa38421ed0cbf2
temimercy <temiloluwa_a@yahoo.com>
2022-09-14 08:12:15 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints alphabet in lower case followed by a new line
- */
+* print_alphabet - Prints alphabet in lower case followed by a new line
+*/
 void print_alphabet(void)
 {
 	char c;
