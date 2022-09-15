4b3b937d10735a0c52b9f43b91a5a3b63b46ce4b
temimercy <temiloluwa_a@yahoo.com>
2022-09-14 19:52:18 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,6 @@
 /**
  * print_to_98 - Prints all natural numbers from n to 98
  * @n: random natural number
- *
  */
 void print_to_98(int n)
 {
@@ -12,9 +11,17 @@ void print_to_98(int n)
 		while (n > 98)
 		{
 			printf("%d, ", n);
+			n--;
+		}
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d\n", n);
 			n++;
 		}
 		printf("%d\n", n);
 	}
 }
-
