f275065445a11ab3c855de23f2ffc6ff6f5d63b1
temimercy <temiloluwa_a@yahoo.com>
2022-09-20 18:33:33 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints shalf a string
+ * @str: pointer to string to print
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int index = 0;
+	int len = 0;
+	int n;
+
+	while (str[index++])
+	len++;
+
+	if ((len % 2) == 0)
+		n = len / 2;
+	else
+		n = (len + 1) / 2;
+
+	for (index = n; index < len; index++)
+	_putchar(str[index]);
+
+	_putchar('\n');
+}
